a26418f2e00738489f302069068301f6141d7d44
krishiv1545 <krishiv1545@gmail.com>
2026-03-12 15:52:53 +0530

    add student page

--- facial_auth/core_APP/modules/admin/urls.py
+++ facial_auth/core_APP/modules/admin/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import admin_dashboard
+from .views import admin_dashboard, add_student, biometric_capture
 
 
 urlpatterns = [
     path("admin-dashboard/", admin_dashboard, name="admin_dashboard"),
+    path("add-student/", add_student, name="add_student"),
+    path("api/biometric-capture/", biometric_capture)
 ]
--- facial_auth/core_APP/modules/admin/views.py
+++ facial_auth/core_APP/modules/admin/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 @login_required
@@ -18,3 +20,31 @@ def admin_dashboard(request):
     })
 
     return render(request, "admin/dashboard.html", context)
+
+
+@login_required
+def add_student(request):
+    if request.user.role != "ORG-ADMIN":
+        return redirect("admin_dashboard")
+    
+    context = {}
+    context.update({"user": request.user, "organization": request.user.organization})
+
+    context.update({
+        "primary": request.user.organization.primary_color or None,
+        "secondary": request.user.organization.secondary_color or None,
+        "tertiary": request.user.organization.tertiary_color or None
+    })
+
+    return render(request, "admin/add_student.html", context)
+
+
+@csrf_exempt
+def biometric_capture(request):
+    if request.user.role != "ORG-ADMIN":
+        return redirect("admin_dashboard")
+    
+    return JsonResponse({
+        "success": True,
+        "message": "Biometric capture successful",
+    })
